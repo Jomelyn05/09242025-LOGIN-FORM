ec3768bea7bf686c3247ffcfae45acdd732e2a40
senku-1 <ojtdevai@comfac-it.com>
2025-09-24 16:58:24 +0800

    Create login form

---
+++ graphics/register.php
@@ -0,0 +1,35 @@
+<?php
+
+include "connect.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+//GET FORM DATA
+    $email = mysqli_real_escape_string($conn , $_POST['email']);
+    $fullname = mysqli_real_escape_string($conn , $_POST['fullname']);
+    $password = mysqli_real_escape_string($conn , $_POST['password']);
+
+    //CHECK IF THE EMAIL IS ALREADY IN DATABASE
+
+    $checkEmail = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($checkEmail);
+
+    if($result->num_rows > 0){
+        echo "Email already has a account";
+    } else {
+
+    //CREATE ACCOUNT //SAVE DATA TO DATABASE
+
+    //SECURITY - HASHING
+    $hashed_password = password_hash($password , PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO users(fullname,email,password) VALUES ('$fullname', '$email','$hashed_password')";
+
+    if($conn->query($sql)===TRUE){
+        echo "ACCOUNT CREATED!!!";
+         } else {
+        echo "ERROR" .$sql.$conn->error;
+        }
+    }
+}
+
+?>
